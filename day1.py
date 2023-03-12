0ff923a2a96080d755459f9bef96068067f26166
Dev <dev@Devs-MacBook-Pro-12.local>
2023-03-12 03:32:33 -0700

    Python Basics: Initial Commit

---
+++ day1.py
@@ -0,0 +1,30 @@
+#  Mathematical operations
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+# Print String
+print('Your name')
+print('Your family name')
+print('Your country')
+print('I am enjoying 30 days of python')
+
+# Datatype
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Test User'))
+print(type('Dummy Example'))
+print(type('Pakistan'))
+
+# Eculedin Distance
+a = input('Enter First Number \t')
+b = input('Enter Second Number \t')
+# d = sqrt(pow((a + b), 2), 2)
+print(f'Hello {a}')
---
+++ iterators.py
@@ -0,0 +1,16 @@
+# for loop with array
+# for iterator in [1,2,3,4,5]:
+#     print(iterator)
+
+# for loop with sting
+# for iterator in 'string':
+#     print(iterator)
+
+# for loop with integer
+# for iterator in range(3,5):
+#     print(iterator)
+
+# for loop with dictionary
+dict = {'a': 1, 'b': 2}
+for iterator in dict:
+    print(dict[iterator])
